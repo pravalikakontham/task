897b21ff2ca9ee8dce4c613e170afa4b0e87fc0d
pravalika <pravalika@rtechsols.in>
2021-11-29 16:12:14 +0530

    commit

---
+++ database/migrations/2021_11_26_130449_create_contacts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+   
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('address');
+            $table->string('mobile');
+
+        });
+    }
+
+    
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
